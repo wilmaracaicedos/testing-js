7c64d82a4ac17f2912b92ce407083558d1912b2d
Wilmar Caicedo <wilmar.caicedo@farmappweb.com>
2023-05-05 19:03:44 -0500

    implementando setup

--- demos/src/02-math.test.js
+++ demos/src/02-math.test.js
@@ -1,27 +1,35 @@
 const { sum, multiply, divide } = require('./02-math');
 
-test('adds 1 + 3 should be 4', () => {
-  const rta = sum(1, 3);
-  expect(rta).toBe(4);
-});
+describe('Test for Math', () => {
+  describe('tests for sum', () => {
+    test('adds 1 + 3 should be 4', () => {
+      const rta = sum(1, 3);
+      expect(rta).toBe(4);
+    });
+  });
 
-test('should be 4', () => {
-  const rta = multiply(1, 4);
-  expect(rta).toBe(4);
-});
+  describe('tests for multiply', () => {
+    test('should be 4', () => {
+      const rta = multiply(1, 4);
+      expect(rta).toBe(4);
+    });
+  });
 
-test('should divide', () => {
-  const rta = divide(6, 3);
-  expect(rta).toBe(2);
+  describe('tests for divide', () => {
+    test('should divide', () => {
+      const rta = divide(6, 3);
+      expect(rta).toBe(2);
 
-  const rta2 = divide(5, 2);
-  expect(rta2).toBe(2.5);
-});
+      const rta2 = divide(5, 2);
+      expect(rta2).toBe(2.5);
+    });
 
-test('should divide for zero', () => {
-  const rta = divide(6, 0);
-  expect(rta).toBeNull();
+    test('should divide for zero', () => {
+      const rta = divide(6, 0);
+      expect(rta).toBeNull();
 
-  const rta2 = divide(5, 0);
-  expect(rta2).toBeNull();
+      const rta2 = divide(5, 0);
+      expect(rta2).toBeNull();
+    });
+  });
 });
