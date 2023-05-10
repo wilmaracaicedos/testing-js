5887ca949a86f128f8c14592754482a34c9500fd
Wilmar Caicedo <wilmar.caicedo@farmappweb.com>
2023-05-10 18:36:33 -0500

    mocking

---
+++ api/src/services/books.service.test.js
@@ -0,0 +1,41 @@
+const BooksService = require('./books.service');
+
+const fakeBooks = [
+  {
+    _id: 1,
+    name: 'Harry Potter',
+  },
+];
+
+const MongoLibStub = {
+  getAll: () => [...fakeBooks],
+  create: () => {},
+};
+
+jest.mock('../lib/mongo.lib', () => jest.fn().mockImplementation(() => MongoLibStub));
+
+describe('Test for BooksService', () => {
+  let service;
+  beforeEach(() => {
+    service = new BooksService();
+    jest.clearAllMocks();
+  });
+
+  describe('test for getBooks', () => {
+    test('should return a list book', async () => {
+      // Act
+      const books = await service.getBooks({});
+      console.log(books);
+      // Assert
+      expect(books.length).toEqual(1);
+    });
+
+    test('should return a list book', async () => {
+      // Act
+      const books = await service.getBooks({});
+      console.log(books);
+      // Assert
+      expect(books[0].name).toEqual('Harry Potter');
+    });
+  });
+});
